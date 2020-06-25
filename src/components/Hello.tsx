65373e89f885af610a76954946ff8ebe097f6b8b
trucopaulista23 <trucopaulista23@gmail.com>
2020-06-25 18:46:26 -0300

    Iniciando o projeto

---
+++ src/components/Hello.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { Text } from 'react-native';
+
+const Hello: FC<any> = () => {
+    return (
+        <Text>Hello</Text>
+    )
+}
+
+export default Hello;
